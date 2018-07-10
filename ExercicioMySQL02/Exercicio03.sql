2779f22fbc55727284b8d55b4ce520b361b84d3c
Leonardo Stephaniakwe <leonardomoreira050@gmail.com>
2018-07-10 09:21:27 -0300

    Exercicios concluidos
    Exercicio 20

--- ExercicioMySQL02/Exercicio03.sql
+++ ExercicioMySQL02/Exercicio03.sql
@@ -17,9 +17,9 @@ SELECT SUM(nota_1) FROM alunos;
 SELECT AVG(nota_2) FROM alunos;
 
 #select07
-SELECT nome, MIN(nota_1) FROM alunos WHERE nota_1 = (SELECT MIN(nota_1) FROM alunos);
+SELECT nome, nota_1 FROM alunos WHERE nota_1 = (SELECT MIN(nota_1) FROM alunos);
 
-#select98
+#select8
 SELECT nome, nota_1, nota_2, nota_3, nota_4 FROM alunos WHERE nome = (SELECT max(nome) FROM alunos) ORDER BY nome, nota_1, nota_2, nota_3, nota_4;
 
 #select09
@@ -27,3 +27,67 @@ SELECT nome, cor_preferida, COUNT(cor_preferida)'Quantidade de alunos' FROM alun
 
 #select10
 SELECT nome FROM alunos WHERE nome LIKE 'Francisco%' ORDER BY nome;
+
+#select11
+SELECT nome FROM alunos WHERE nome LIKE '%Luc%' ORDER BY nome;
+
+#select12
+SELECT nome, signo, data_nascimento FROM alunos WHERE signo LIKE '%Aries%' ;
+
+#select 13
+SELECT nota_1, nota_2, nota_3, nota_4 FROM alunos WHERE (nota_1 + nota_2 + nota_3 + nota_4)/4 = 
+(SELECT MAX((nota_1 + nota_2 + nota_3 + nota_4)/4) FROM alunos);
+
+#select 14
+SELECT nome'Nome', nota_1, nota_2, nota_3, nota_4,(nota_1 + nota_2 + nota_3 + nota_4)/4, 
+IF ( (nota_1 + nota_2 + nota_3 + nota_4)/4 < 5, 'Reprovado',
+IF ( (nota_1 + nota_2 + nota_3 + nota_4)/4 < 7, 'Em exame','Aprovado')) 
+FROM alunos 
+WHERE ((nota_1 + nota_2 + nota_3 + nota_4)/4);
+
+
+#select 15
+SELECT nome, nota_1, nota_2, nota_3, nota_4 FROM alunos WHERE (nota_1 + nota_2 + nota_3 + nota_4)/4 = 
+(SELECT MIN((nota_1 + nota_2 + nota_3 + nota_4)/4) FROM alunos);
+
+
+#select 16
+SELECT nome'Nome', nota_1, nota_2, nota_3, nota_4, CAST(((nota_1 + nota_2 + nota_3 + nota_4)/4) AS DECIMAL(5,2))'Média' FROM alunos 
+WHERE ((nota_1 + nota_2 + nota_3 + nota_4)/4) > 7;
+
+#select 17
+SELECT nick FROM alunos WHERE length(nick) = 5 ORDER BY nome asc;
+
+#select 18
+SELECT nome 'Nome', nick 'Nick', cor_preferida 'Cor Preferida', CAST(((nota_1 + nota_2 + nota_3 + nota_4)/4) AS DECIMAL(5,2))'Média' FROM alunos 
+WHERE cor_preferida LIKE 'Amarelo-Torrado' OR cor_preferida LIKE 'Rosa' AND ((nota_1 + nota_2 + nota_3 + nota_4)/4) > 6.5;
+
+#select 19
+SELECT data_nascimento 'Nascimento', YEAR(data_nascimento)'Ano de Nascimento' FROM alunos;
+
+#select20
+SELECT data_nascimento 'Nascimento', MONTH(data_nascimento)'Mês de Nascimento' FROM alunos;
+
+#select21
+SELECT nome, data_nascimento 'Nascimento', COUNT(data_nascimento LIKE '1996%') 'Ano de preferencia' FROM alunos;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
